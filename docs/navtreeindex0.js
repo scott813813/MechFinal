27f5701e4b59d5e8aa8ac5a8cec55eb3de142249
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-02-28 19:21:23 -0800

    Add doxygen, ReadMe, and mainpage

---
+++ docs/navtreeindex0.js
@@ -0,0 +1,14 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[1,0,0],
+"files.html":[1,0],
+"index.html":[],
+"index.html#Button":[0,0],
+"index.html#Camera":[0,1],
+"index.html#Firing":[0,4],
+"index.html#Pitch":[0,3],
+"index.html#Software":[0],
+"index.html#Yaw":[0,2],
+"mainpage_8py.html":[1,0,0,0],
+"pages.html":[]
+};
