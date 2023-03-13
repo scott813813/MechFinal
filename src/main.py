0246a5a15a2f2f7b229a71dd19c45ede0575f8a3
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-03-13 10:23:03 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,116 +18,24 @@ from closed_loop_control import clCont # The closed loop control method from clo
 from motor_driver import MotorDriver # The method to drive the motor from motor_drive.py
 from encoder_reader import EncoderReader # Read encoder method from encoder_reader.py
 
-def main():
-    ''' Yaw Setup Below'''
-    pinA10 = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
-    pinB4 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-    pinB5 = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
-    tim1 = 3
-    moeY = MotorDriver(pinA10,pinB4,pinB5,tim1)
-    
-    ''' Yaw Encoder Setup Below'''
-    pinB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
-    pinB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
-    encY = EncoderReader(pinB6, pinB7, 4)
-    encY.zero()
-    
-    ''' Pitch Setup Below'''
-    pinC1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
-    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
-    pinA1 = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP)
-    tim2 = 5
-    moeP = MotorDriver(pinC1,pinA0,pinA1,tim2)
-    
-    ''' Pitch Encoder Setup Below'''
-    pinC6 = pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN)
-    pinC7 = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.IN)
-    encP = EncoderReader(pinC6, pinC7, 8)
-    encP.zero()
-    
-    # Initialized values
-    masterState = 0 # Initialized state of FSM
-    fireState = 0 # Initialized state of FSM
-    
-    zeroPoint = utime.ticks_ms() # Set inital time of the program
-    trackPeriod = 4000 # ms, the time between the start and tracking
-    firePeriod = 5000 # ms, the time between the start and firing
-    idlePeriod = 15000 # ms, the time between the start and the end of firing
-    
-    pinFlywheel = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP) # Flywheel port
-    
-    yawStartPos = 13272 # 180 degrees, ie 3.32 rotations with a gear ratio of 15
-    pitchStartPos = 0 # Keep steady heading
-    global buttonGo
-    buttonGo = False
-    global buttonCounts
-    buttonCounts = 0
-
-    # Run the scheduler with the chosen scheduling algorithm. Quit if ^C pressed
-    print('We here 1')
-    
-    
-    # Create a set of shares detailing the statuse of the pitch and firing states
-    s_YawPos = task_share.Share('i', thread_protect=False, name="Yaw Position")
-    s_PitchPos = task_share.Share('i', thread_protect=False, name="Pitch Position")
-    s_YawOnTarg = task_share.Share('b', thread_protect=False, name="Yaw On Target")
-    s_PitchOnTarg = task_share.Share('b', thread_protect=False, name="Pitch On Target")
-    s_TimeToTrack = task_share.Share('b', thread_protect=False, name="Time To Track")
-    s_TimeToFire = task_share.Share('b', thread_protect=False, name="Time To Fire")
-    s_StopShooting = task_share.Share('b', thread_protect=False, name="Stop Shooting")
-    
-    # Create the tasks. If trace is enabled for any task, memory will be
-    # allocated for state transition tracing, and the application will run out
-    # of memory after a while and quit. Therefore, use tracing only for 
-    # debugging and set trace to False when it's not needed
-    task1 = cotask.Task(masterTask, name="Master Task", priority=1, period=10,
-                        profile=True, trace=True, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
-    task2 = cotask.Task(yawTask, name="Yaw Task", priority=2, period=40,
-                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
-    '''task3 = cotask.Task(pitchTask, name="Pitch Task", priority=3, period=40,
-                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
-    task4 = cotask.Task(pictureTask, name="Picture Task", priority=5, period=40,
-                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
-    task5 = cotask.Task(fireTask, name="Fire Task", priority=4, period=100,
-                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
-                        '''
-    
-    cotask.task_list.append(task1)
-    cotask.task_list.append(task2)
-    '''
-    cotask.task_list.append(task3)
-    cotask.task_list.append(task4)
-    cotask.task_list.append(task5)
-    '''
-    
-    # Run the memory garbage collector to ensure memory is as defragmented as
-    # possible before the real-time scheduler is started
-    gc.collect()
-    print('here 2')
-    
     
 def buttonLogic(pin):
-    
-    buttonCounts += 1
     print('button press')
-    
-    if buttonCounts == 1:
-        buttonGo = True
-
-    else:
-        buttonGo = False
-        
-    
-    #if buttonSet == True:   
-    #buttonState = False
+    global buttonCounts
+    buttonCounts += 1
+    print(buttonCounts)
 
 
 
 def masterTask(shares):
     s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting = shares
+    masterState = 0
+    #yawStartPos = 13272 # 180 degrees, ie 3.32 rotations with a gear ratio of 15
+    #pitchStartPos = 0 # Keep steady heading
     while True:
+        print('button', buttonCounts)
         print('master')
-        while buttonGo == True:
+        while buttonCounts == 1:
             if masterState == 0: # Initilization state
                 masterState = 1
                 print('Master: S1')
@@ -135,7 +43,7 @@ def masterTask(shares):
                 
             elif masterState == 1: # Go to aiming position  
                 s_YawPos.put(yawStartPos)
-                #s_PitchPos.put(pitchStartPos)
+                s_PitchPos.put(pitchStartPos)
                 #masterState = 2
                 print('Master: S2')
                 yield
@@ -163,16 +71,16 @@ def yawTask(shares):
     s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting = shares
     while True:
         print('yaw')
-        if buttonGo == True:
+        if buttonCounts == 1:
             '''Control Loop Setup'''
             Kp = 0.06				#0.1 excessive oscillation,  0.005 good performance, 0.002 underdamped
             cll = clCont(0, Kp) # Set proportional constant gain for yaw motor
-            print('Set Motor constant')
-            while True:
-                p = encY.read() # Current position of yaw motor 
+            print('Set Yaw Motor constant')
+            while buttonCounts == 1:
+                p = encY.read() # Current position of yaw motor
                 lvl = cll.run(s_YawPos.get(), p) # Run closed loop controller
-                moey.set_duty_cycle(lvl) # Set the duty cycle
-                print('Motor')
+                moeY.set_duty_cycle(lvl) # Set the duty cycle
+                print('Yaw Motor')
                 # send new value high, 
                 # scrub current reading, and set it to 0
                 # go to new reading, and set new value reading low
@@ -180,24 +88,27 @@ def yawTask(shares):
             yield
         yield
                 
-'''    
+
 def pitchTask(shares): 
     s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting = shares
-    
-    if buttonGo == True:
-        #Control Loop Setup
-        Kp = 0.06				#0.1 excessive oscillation,  0.005 good performance, 0.002 underdamped
-        cll = clCont(0, Kp) # Set proportional constant gain for yaw motor
-        
-        while True:
-            p = encP.read() # Current position of pitch motor 
-            lvl = cll.run(s_PitchPos.get(), p) # Run closed loop controller
-            moey.set_duty_cycle(lvl) # Set the duty cycle
-            # send new value high, 
-            # scrub current reading, and set it to 0
-            # go to new reading, and set new value reading low
-'''    
-
+    while True:
+        print('pitch')
+        if buttonCounts == 1:
+            '''Control Loop Setup'''
+            Kp = 0.06				#0.1 excessive oscillation,  0.005 good performance, 0.002 underdamped
+            cll = clCont(0, Kp) # Set proportional constant gain for yaw motor
+            print('Set Yaw Motor constant')
+            while buttonCounts == 1:
+                p = encP.read() # Current position of pitch motor 
+                lvl = cll.run(s_PitchPos.get(), p) # Run closed loop controller
+                moeP.set_duty_cycle(lvl) # Set the duty cycle
+                print('Pitch Motor')
+                # send new value high, 
+                # scrub current reading, and set it to 0
+                # go to new reading, and set new value reading low
+                yield
+            yield
+        yield
 '''
 def pictureTask(shares):
     s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting = shares
@@ -208,14 +119,14 @@ def pictureTask(shares):
     
     while buttonGo == True:
         if s_TimeToTrack.get() == True: # Only aim if given flag to aim
-            if yawDif == 0:
+            if yawDif <= 30:
                 s_YawOnTarg.put(True)
             else:
                 s_YawOnTarg.put(False)
                 yawPos = yawDif + yawStartPos
                 s_YawPos.put(yawPos)
                 
-            if pitchDif == 0:
+            if pitchDif <= 30:
                 s_PitchOnTarg.put(True)
             else:
                 s_PitchOnTarg.put(False)
@@ -264,8 +175,96 @@ buttonInt = pyb.ExtInt(pyb.Pin.board.PC13, pyb.ExtInt.IRQ_FALLING, pyb.Pin.PULL_
 
 
 if __name__ == "__main__":
+
     
-    main()
+    ''' Yaw Setup Below'''
+    pinC1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
+    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+    pinA1 = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP)
+    tim2 = 5
+    moeY = MotorDriver(pinC1,pinA0,pinA1,tim2)
+    
+    ''' Yaw Encoder Setup Below'''
+    pinB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+    pinB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
+    encY = EncoderReader(pinB6, pinB7, 4)
+    encY.zero()
+    
+    ''' Pitch Setup Below'''
+    pinA10 = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
+    pinB4 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+    pinB5 = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+    tim1 = 3
+    moeP = MotorDriver(pinA10,pinB4,pinB5,tim1)
+    
+    ''' Pitch Encoder Setup Below'''
+    pinC6 = pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN)
+    pinC7 = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.IN)
+    encP = EncoderReader(pinC6, pinC7, 8)
+    encP.zero()
+    
+    # Initialized values
+    masterState = 0 # Initialized state of FSM
+    fireState = 0 # Initialized state of FSM
+    
+    zeroPoint = utime.ticks_ms() # Set inital time of the program
+    trackPeriod = 4000 # ms, the time between the start and tracking
+    firePeriod = 5000 # ms, the time between the start and firing
+    idlePeriod = 15000 # ms, the time between the start and the end of firing
+    
+    pinFlywheel = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP) # Flywheel port
+    
+    yawStartPos = 13272 # 180 degrees, ie 3.32 rotations with a gear ratio of 15
+    
+    pitchStartPos = 2000 # Keep steady heading
+    #global buttonGo
+    #buttonGo = False
+    global buttonCounts
+    buttonCounts = 0
+
+    # Run the scheduler with the chosen scheduling algorithm. Quit if ^C pressed
+    print('We here 1')
+    
+    
+    # Create a set of shares detailing the statuse of the pitch and firing states
+    s_YawPos = task_share.Share('i', thread_protect=False, name="Yaw Position")
+    s_PitchPos = task_share.Share('i', thread_protect=False, name="Pitch Position")
+    s_YawOnTarg = task_share.Share('b', thread_protect=False, name="Yaw On Target")
+    s_PitchOnTarg = task_share.Share('b', thread_protect=False, name="Pitch On Target")
+    s_TimeToTrack = task_share.Share('b', thread_protect=False, name="Time To Track")
+    s_TimeToFire = task_share.Share('b', thread_protect=False, name="Time To Fire")
+    s_StopShooting = task_share.Share('b', thread_protect=False, name="Stop Shooting")
+    
+    # Create the tasks. If trace is enabled for any task, memory will be
+    # allocated for state transition tracing, and the application will run out
+    # of memory after a while and quit. Therefore, use tracing only for 
+    # debugging and set trace to False when it's not needed
+    task1 = cotask.Task(masterTask, name="Master Task", priority=1, period=10,
+                        profile=True, trace=True, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
+    task2 = cotask.Task(yawTask, name="Yaw Task", priority=2, period=40,
+                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
+    task3 = cotask.Task(pitchTask, name="Pitch Task", priority=3, period=40,
+                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
+    '''
+    task4 = cotask.Task(pictureTask, name="Picture Task", priority=5, period=40,
+                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
+    task5 = cotask.Task(fireTask, name="Fire Task", priority=4, period=100,
+                        profile=True, trace=False, shares=(s_YawPos, s_PitchPos, s_YawOnTarg, s_PitchOnTarg, s_TimeToTrack, s_TimeToFire, s_StopShooting))
+                        '''
+    
+    cotask.task_list.append(task1)
+    cotask.task_list.append(task2)
+    cotask.task_list.append(task3)
+    '''
+    cotask.task_list.append(task4)
+    cotask.task_list.append(task5)
+    '''
+    
+    # Run the memory garbage collector to ensure memory is as defragmented as
+    # possible before the real-time scheduler is started
+    gc.collect()
+    print('here 2')
+    
     while True:
         try:
             cotask.task_list.pri_sched()
