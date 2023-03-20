656691c87a577dc3d2e437924d08e03aee2971b7
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-03-20 15:20:03 -0700

    Update documentation

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,10 @@
 var index =
 [
-    [ "Software Design", "index.html#Design", null ]
+    [ "Software Design", "index.html#Design", [
+      [ "Master Task", "index.html#MasterTask", null ],
+      [ "Yaw Task", "index.html#YawTask", null ],
+      [ "Pitch Task", "index.html#PitchTask", null ],
+      [ "Camera Task", "index.html#CameraTask", null ],
+      [ "Firing Task", "index.html#FiringTask", null ]
+    ] ]
 ];
