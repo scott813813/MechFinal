a11318b51dc97249df679278006403fae8000029
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-03-20 15:43:12 -0700

    Update Dox

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,6 @@
 var index =
 [
     [ "Software Design", "index.html#Design", [
-      [ "Master Task", "index.html#Master_Task", null ],
       [ "Yaw Task", "index.html#Yaw_Task", null ],
       [ "Pitch Task", "index.html#Pitch_Task", null ],
       [ "Camera Task", "index.html#Camera_Task", null ],
