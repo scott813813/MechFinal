09053ff75c86a920400950033e793440c6919beb
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-03-20 13:56:10 -0700

    Update Dox

--- src/mainpage.py
+++ src/mainpage.py
@@ -1,8 +1,8 @@
 ## @file mainpage.py
-#  
+#  Details the tasks and states for the main python file
 #  @ author: mecha12
 #  
-# @section Software_Design Software Design
+# @section Design Software Design
 # The dueling robot uses a set of tasks to run and target an opponent 
 # succesfully. The robot uses MicroPython to control two motors and adjust the
 # pitch and yaw axis in order to follow the target found in the provided
