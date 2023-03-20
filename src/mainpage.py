f8f79b59b9d76ee281cd9596151cafb92175293e
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-03-20 15:38:28 -0700

    Update Dox

--- src/mainpage.py
+++ src/mainpage.py
@@ -27,4 +27,34 @@
 # Reference: 
 # \image html Master_Task_FSM.jpg width=800px
 # 
+# \subsection Yaw_Task Yaw Task
+# The yaw task takes the current horizontal position and uses the control loop
+# developed in lab 2 to try to aim at the target. If Yaw Task is at the same
+# position as the target, then the target is located in the horizontal axis and
+# the Yaw Task shares Y_OnTarg to high to the Firing Task.
+# 
+# \subsection Pitch_Task Pitch Task
+# The pitch task takes the current vertical position and uses the control loop
+# developed in lab 2 to try to aim at the target. If Pitch Task is at the same 
+# position of the target, then the target is located in the vertical axis and
+# the Pitch Task shares P_OnTarg to high to the Firing Task.
+#
+# \subsection Camera_Task Camera Task
+# The camera task queues the location of the target in its field of view with
+# both the Yaw Axis Controller and the Pitch Axis Controller. It utilizes a 
+# an algorithm that 'splits' the 32 by 24 pixel grid into an 8 by 24 grid of 
+# 'arrays' (effectively splits them as so, but not technically). It then 
+# compares the average heat value of each split array to find the maximum
+# and returns its x and y location relative to the center axis of the 
+# screen.
+# 
+# \subsection Firing_Task Firing Task
+# The firing task takes the data shared by both the Yaw Task and the Pitch Task
+# in the form of the two booleans: Y_OnTarg and P_On_Targ respectively. If 
+# both booleans are True then the firing task commands the servo motor to 
+# articulate a dart into the rotating flywheels. 
+#
+# Reference: 
+# \image html Fire_Task_FSM.jpg width=800px
+#
 
